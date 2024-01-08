7309f3311ddcb837ddf159c4c587d7fb77e40619
VitinLima <aguirra.vitor@gmail.com>
2024-01-07 21:27:11 -0300

    saving current system as testing for making large changes

---
+++ testing/EByteSystem/Satellite/SatComm.h
@@ -0,0 +1,43 @@
+#ifndef SAT_COMM_H
+#define SAT_COMM_H
+
+extern uint8_t rxAddh;
+extern uint8_t rxAddl;
+extern uint8_t rxChan;
+extern uint8_t txAddh;
+extern uint8_t txAddl;
+extern uint8_t txChan;
+
+extern unsigned int comm_pointer;
+
+#define N 58 // Size of the added array (+1 for an end of line char)  
+struct Message{
+  unsigned int length;
+  char type[10];
+  char message[13];
+  bool actuator_1;
+  bool actuator_2;
+  bool actuator_3;
+  bool actuator_4;
+}; // sending a struct with multiple fields
+
+struct Telemetry{
+  unsigned int length;
+  char type[10];
+  char telemetry_message[16];
+  bool instrument_1;
+  bool instrument_2;
+  bool instrument_3;
+  bool instrument_4;
+  uint8_t data[N+1];
+};
+
+extern Message message;
+extern Telemetry telemetry;
+
+void updateRFComm();
+
+void sendTelemetry();
+void satReceiveCallback();
+
+#endif
---
+++ testing/EByteSystem/Satellite/Satellite.ino
@@ -0,0 +1,114 @@
+#include <EbyteLib.h>
+#include <Transmission.h>
+#include <Utils.h>
+#include <configurations.h>
+#include <debugging.h>
+#include <definitions.h>
+#include <modes.h>
+
+#include "SatComm.h"
+
+void setup(){
+  Serial.begin(57600);
+
+  Serial.println("Testing Gama Satellite communication system with EByte32 rf module");
+
+  initE32();
+  Serial.println("Device initiated successfully");
+
+  setHEAD(SAVE_ON_POWER_DOWN);
+  setADDH(rxAddh);
+  setADDL(rxAddl);
+  setChannel(rxChan);
+  setParity(UART_PARITY_BIT_8N1);
+  setBaudRate(TTL_UART_BAUD_RATE_57600);
+  setAirDataRate(AIR_DATA_RATE_19200);
+  setTransmissionMode(FIXED_TRANSMISSION_MODE);
+  setIODriveMode(IO_DRIVE_MODE_PUSH_PULL);
+  setWirelessWakeUpTime(WIRELESS_WAKE_UP_TIME_250ms);
+  setFECSwitch(FEC_SWITCH_ON);
+  setTransmissionPower(TRANSMISSION_POWER_20dBm);
+  setConfiguration();
+  
+  for(uint8_t i = 0; i < N; i++){ // Initialize array
+    telemetry.data[i] = 0xA1;
+  }
+  telemetry.data[N] = (uint8_t)'\n';
+  
+  printConfiguration();
+  setNormalMode();
+}
+
+String receiving_serial = "";
+String received_serial = "";
+bool serial_received = false;
+
+bool state_sending = false;
+
+void loop(){
+  checkSerial();
+  updateRFComm();
+
+  delay(500);
+}
+
+void checkSerial(){
+  char c;
+  while(Serial.available() && !serial_received){
+    c = Serial.read();
+    if(c=='\n'){
+      received_serial = receiving_serial;
+      receiving_serial = "";
+      serial_received = true;
+    } else{
+      receiving_serial += c;
+    }
+  }
+
+  if(serial_received){
+    serial_received = false;
+    if(received_serial.length()==0){
+      // state_sending = !state_sending;
+      Serial.println("Sending telemetry");
+      sendTelemetry();
+      // Serial.print("Toggled communication ");
+      // Serial.println(state_sending ? "ON":"OFF");
+    } else if(received_serial[0] == 'T'){
+      if(received_serial.startsWith("Tset tx chan ")){
+        txChan = received_serial.substring(13).toInt();
+        Serial.print("Set tx channel to ");
+        Serial.println(txChan);
+      } else if(received_serial.startsWith("Tset tx addh ")){
+        txAddh = received_serial.substring(13).toInt();
+        Serial.print("Set tx addh to ");
+        Serial.println(txAddh);
+      } else if(received_serial.startsWith("Tset tx addl ")){
+        txAddl = received_serial.substring(13).toInt();
+        Serial.print("Set tx addl to ");
+        Serial.println(txAddl);
+      }
+    } else if(received_serial[0] == 'G'){
+      if(received_serial.startsWith("GAddh")){
+        printADDH();
+      } else if(received_serial.startsWith("GAddl")){
+        printADDL();
+      } else if(received_serial.startsWith("GParity")){
+        printParity();
+      } else if(received_serial.startsWith("GAirDataRate")){
+        printAirDataRate();
+      } else if(received_serial.startsWith("GBaudRate")){
+        printBaudRate();
+      } else if(received_serial.startsWith("GChannel")){
+        printChannel();
+      } else if(received_serial.startsWith("GTxPower")){
+        printTransmissionPower();
+      }
+    } else if(received_serial[0] == 'S'){
+      parseMessage(received_serial.substring(1));
+    } else if(received_serial[0] == 'M'){
+      Serial.println("Set telemetry index to 0");
+      comm_pointer = 0;
+    }
+  }
+}
+
