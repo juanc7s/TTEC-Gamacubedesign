b4ee7e033b44c668188c248b390e2cb90a181311
VitinLima <aguirra.vitor@gmail.com>
2024-02-16 23:20:23 -0300

    Implemented main tt&c code

--- main/Satellite/SatTTC.cpp
+++ main/Satellite/SatTTC.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <thread>
+#include <filesystem>
+#include <iostream>
+#include <sys/statvfs.h>
+#include <cstring>
 using namespace std;
 
 #ifdef __cplusplus
@@ -8,9 +13,11 @@ using namespace std;
 #endif
 
 #include "Logger.h"
+#include "RFModule.h"
+#include "Timing.h"
 
-LoRa_ctl modem;
-Logger logger;
+#include "StatusServer.cpp"
+#include "ImagingServer.cpp"
 
 unsigned long int status_write_period = 500;
 unsigned long int imaging_write_period = 250;
@@ -19,125 +26,181 @@ unsigned long int imaging_write_time = 0;
 
 bool enable_writing = false;
 
-void * rx_f(void *p){
-    rxData *rx = (rxData *)p;
-    printf("rx done \n");
-    printf("CRC error: %d\n", rx->CRC);
-    printf("Data size: %d\n", rx->size);
-    printf("string: %s\n", rx->buf);//Data we'v received
-    printf("RSSI: %d\n", rx->RSSI);
-    printf("SNR: %f\n", rx->SNR);
-    free(p);
-    return NULL;
+bool running = true;
+
+ImagingFIFO imaging_fifo;
+StatusFIFO status_fifo;
+
+ImagingDataServer server_1(&imaging_fifo, 8080);
+StatusDataServer server_2(&status_fifo, 8081);
+
+void run_server_1(){
+  server_1.run_server();
 }
 
-void tx_f(txData *tx){
-    printf("tx done \n");
+void run_server_2(){
+  server_2.run_server();
 }
 
-void initRFModule(){
-  char txbuf[255];
-
-  //See for typedefs, enumerations and there values in LoRa.h header file
-  modem.spiCS = 0;//Raspberry SPI CE pin number
-  modem.tx.callback = tx_f;
-  modem.tx.data.buf = txbuf;
-  memcpy(modem.tx.data.buf, "LoRa", 5);//copy data we'll sent to buffer
-  modem.tx.data.size = 5;//Payload len
-  modem.eth.preambleLen=6;
-  modem.eth.bw = BW62_5;//Bandwidth 62.5KHz
-  modem.eth.sf = SF12;//Spreading Factor 12
-  modem.eth.ecr = CR8;//Error coding rate CR4/8
-  modem.eth.CRC = 1;//Turn on CRC checking
-  modem.eth.freq = 434800000;// 434.8MHz
-  modem.eth.resetGpioN = 4;//GPIO4 on lora RESET pin
-  modem.eth.dio0GpioN = 17;//GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
-  modem.eth.outPower = OP20;//Output power
-  modem.eth.powerOutPin = PA_BOOST;//Power Amplifire pin
-  modem.eth.AGC = 1;//Auto Gain Control
-  modem.eth.OCP = 240;// 45 to 240 mA. 0 to turn off protection
-  modem.eth.implicitHeader = 0;//Explicit header mode
-  modem.eth.syncWord = 0x12;
-  //For detail information about SF, Error Coding Rate, Explicit header, Bandwidth, AGC, Over current protection and other features refer to sx127x datasheet https://www.semtech.com/uploads/documents/DS_SX1276-7-8-9_W_APP_V5.pdf
-
-  LoRa_begin(&modem);
-}
-
-void tx_send(){
-  LoRa_send(&modem);
-
-  printf("Tsym: %f\n", modem.tx.data.Tsym);
-  printf("Tpkt: %f\n", modem.tx.data.Tpkt);
-  printf("payloadSymbNb: %u\n", modem.tx.data.payloadSymbNb);
-
-  printf("sleep %d seconds to transmitt complete\n", (int)modem.tx.data.Tpkt/1000);
-  sleep(((int)modem.tx.data.Tpkt/1000)+1);
-
-  printf("end\n");
-
-  LoRa_end(&modem);
-}
-
-void write_status_data(){
-  HealthData he = {
-    .index = writing_status_counter,
-    .time = 1,
-    .battery_voltage = 3.7,
-    .battery_current = 2.0,
-    .battery_charge = 100.0,
-    .battery_temperature = 33.0,
-    .internal_temperature = 37.9,
-    .external_temperature = 51.2,
-    .sd_memory_usage = (unsigned long)3,
-    .rasp_data = {0xa1, 0xa1, 0xa1, 0xa1, 0xa1, 0xa1, 0xa1, 0xa1, 0xa1, 0xa1},
-  };
-  logger.writeSatStatusPacket(he);
-}
-void write_imaging_data(){
-  ImagingData im;
-  for(int i = 0 ; i < 5; i++){
-    im.lightnings[i].index = writing_imaging_counter;
-    im.lightnings[i].duration = 5;
-    im.lightnings[i].radius = 1;
-    im.lightnings[i].x = i*10;
-    im.lightnings[i].y = i*20;
+void read_fifos(){
+  while(imaging_fifo.available()){
+    ImagingData imagingPacket = imaging_fifo.read();
+    logger.writeSatImagingDataPacket(imagingPacket);
+    // cout << "Imaging FIFO: " << endl;
+    // for(int i = 0; i < 5; i++){
+    //   cout << "Index: " << newPacket.lightnings[i].index << endl;
+    //   cout << "Duration: " << newPacket.lightnings[i].duration << endl;
+    //   cout << "Size: " << newPacket.lightnings[i].radius << endl;
+    //   cout << "x: " << newPacket.lightnings[i].x << endl;
+    //   cout << "y: " << newPacket.lightnings[i].y << endl << endl;
+    // }
+    // cout << endl;
   }
-  logger.writeSatImagingDataPacket(im);
+  while(status_fifo.available()){
+    HealthData statusPacket = status_fifo.read();
+    // cout << "Packet bytes: " << endl;
+    // for(int i = 0; i < sizeof(HealthData); i++){
+    //   cout << (int)(((uint8_t*)&statusPacket)[i]) << " ";
+    // }
+    // cout << endl; 
+    logger.writeSatStatusPacket(statusPacket);
+  }
 }
 
-void loop(){
-  // checkSerial();
-  // updateRFComm();
+uint8_t serial_buffer[256];
+unsigned int serial_buffer_writing_pointer = 0;
+unsigned int serial_buffer_reading_pointer = 0;
+unsigned int serial_buffer_size = 0;
 
-  // unsigned long int t = millis();
-  // if(t > status_write_time){
-  //   status_write_time += status_write_period;
-  //   if(enable_writing){
-  //     std::cout << "Writing status data";
-      // write_status_data();
-  //   }
-  // }
-  // if(t > imaging_write_time){
-  //   imaging_write_time += imaging_write_period;
-  //   if(enable_writing){
-  //     std::cout << "Writing imaging data";
-  //     // write_imaging_data();
-  //   }
-  // }
+void serial_receiving_thread(){
+  char c;
+  while(running){
+    std::cin >> c;
+    if(c=='e'){
+      running = false;
+    } else{
+      serial_buffer[serial_buffer_writing_pointer++] = c;
+      if(serial_buffer_writing_pointer==256){
+        serial_buffer_writing_pointer = 0;
+      }
+      serial_buffer_size++;
+    }
+  }
 }
 
-/*
+uint8_t serial_available(){
+  return serial_buffer_size;
+}
+
+uint8_t serial_read(){
+  serial_buffer_size--;
+  uint8_t b = serial_buffer[serial_buffer_reading_pointer++];
+  if(serial_buffer_reading_pointer==256){
+    serial_buffer_reading_pointer = 0;
+  }
+  return b;
+}
+
 void checkSerial(){
   char c;
-  while(Serial.available()){
-    c = Serial.read();
-    if(c=='0'){
-      enable_writing = !enable_writing;
-    } else if(c=='1'){
-      switch_status_file();
-    } else if(c=='2'){
-      switch_imaging_file();
-    }
+  // while(serial_available()){
+  //   c = serial_read();
+  //   GSPacket sending_packet;
+  //   switch(c){
+  //     case '0':
+  //       std::cout << "Enabling writing" << std::endl;
+  //       enable_writing = !enable_writing;
+  //       break;
+  //     case '1':
+  //       std::cout << "Switching status file" << std::endl;
+  //       logger.switch_status_file();
+  //       break;
+  //     case '2':
+  //       std::cout << "Switching imaging data file" << std::endl;
+  //       logger.switch_imaging_file();
+  //       break;
+  //     case '3':
+  //       std::cout << "Sending status request" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_STATUS;
+  //       sending_packet.operation.operation = GS_STATUS_REQUEST;
+  //       sending_packet.length = 2;
+  //       // std::cout << "Sending GS packet with length " << (int)sending_packet.length << std::endl;
+  //       // std::cout << "Protocol: " << sending_packet.operation.protocol << std::endl;
+  //       // std::cout << "Operation: " << sending_packet.operation.operation << std::endl;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '4':
+  //       std::cout << "Sending status start transmission" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_STATUS;
+  //       sending_packet.operation.operation = GS_STATUS_START_TRANSMISSION;
+  //       sending_packet.length = 2;
+  //       // std::cout << "Sending GS packet with length " << (int)sending_packet.length << std::endl;
+  //       // std::cout << "Protocol: " << sending_packet.operation.protocol << std::endl;
+  //       // std::cout << "Operation: " << sending_packet.operation.operation << std::endl;
+  //       // std::cout << "Writing to rx buffer: ";
+  //       // for(int i = 0; i < sending_packet.length; i++){
+  //       //   std::cout << (int)((uint8_t*)&sending_packet)[i] << " ";
+  //       // }
+  //       // std::cout << std::endl;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '5':
+  //       std::cout << "Sending resend status" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_STATUS;
+  //       sending_packet.operation.operation = GS_STATUS_RESEND_PACKET;
+  //       for(unsigned int i = 0; i < 32; i++){
+  //         sending_packet.data.resend.packets[i] = 0;
+  //       }
+  //       sending_packet.data.resend.packets[0] |= 0x2;
+  //       // sending_packet.data.resend.packets[10] |= 0x10;
+  //       // sending_packet.data.resend.packets[23] |= 0x8;
+  //       gsPacket.data.resend.isDone = false;
+  //       sending_packet.length = 35;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '6':
+  //       std::cout << "Sending status done" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_STATUS;
+  //       sending_packet.operation.operation = GS_STATUS_DONE;
+  //       sending_packet.length = 2;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '7':
+  //       std::cout << "Sending imaging request" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_IMAGING_DATA;
+  //       sending_packet.operation.operation = GS_IMAGING_REQUEST;
+  //       sending_packet.length = 2;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '8':
+  //       std::cout << "Sending status start transmission" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_IMAGING_DATA;
+  //       sending_packet.operation.operation = GS_IMAGING_START_TRANSMISSION;
+  //       sending_packet.length = 2;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case '9':
+  //       std::cout << "Sending resend status" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_IMAGING_DATA;
+  //       sending_packet.operation.operation = GS_IMAGING_RESEND_STATUS;
+  //       for(unsigned int i = 0; i < 32; i++){
+  //         gsPacket.data.resend.packets[i] = 0;
+  //       }
+  //       gsPacket.data.resend.packets[2] |= 0x2;
+  //       gsPacket.data.resend.packets[10] |= 0x10;
+  //       gsPacket.data.resend.packets[23] |= 0x8;
+  //       gsPacket.data.resend.isDone = false;
+  //       sending_packet.length = 35;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+  //     case 'a':
+  //       std::cout << "Sending status done" << std::endl;
+  //       sending_packet.operation.protocol = PROTOCOL_IMAGING_DATA;
+  //       sending_packet.operation.operation = GS_IMAGING_DONE;
+  //       sending_packet.length = 2;
+  //       modem_write((uint8_t*)&sending_packet, sending_packet.length);
+  //       break;
+    // }
     // if(c=='\n'){
     //   received_serial = receiving_serial;
     //   receiving_serial = "";
@@ -145,7 +208,7 @@ void checkSerial(){
     // } else{
     //   receiving_serial += c;
     // }
-  // }
+  }
 
   // if(serial_received){
   //   serial_received = false;
@@ -191,28 +254,62 @@ void checkSerial(){
   //     Serial.println("Set telemetry index to 0");
   //     comm_pointer = 0;
   //   }
-  }
+  // }
 }
 
-*/
+void loop(){
+  checkSerial();
+  updateRFComm();
+  read_fifos();
 
-void init_sd_logger(){
+  // unsigned long int t = millis();
+  // if(t > status_write_time){
+  //   status_write_time += status_write_period;
+  //   if(enable_writing){
+  //     std::cout << "Writing status data" << std::endl;
+  //     write_status_data();
+  //   }
+  // }
+  // if(t > imaging_write_time){
+  //   imaging_write_time += imaging_write_period;
+  //   if(enable_writing){
+  //     std::cout << "Writing imaging data" << std::endl;
+  //     // write_imaging_data();
+  //   }
+  // }
 }
 
-int main(){
+int main( int argc, char *argv[] ){
   std::cout << "Testing Raspberry Gama Satellite communication system with LoRa Ra-01 rf module\n";
 
   initRFModule();
   std::cout << "Device initiated successfully\n";
 
-  init_sd_logger();
-  std::cout << "Logger initiated successfully\n";
+  std::thread thread_1 (run_server_1);
+  std::thread thread_2 (run_server_2);
   
   // setReceiveCallback(onReceive);
-
-  // while(running){
-  //   loop();
+  // for(int i = 0; i < 30; i++){
+  //   write_status_data();
+  // }
+  // for(int i = 0; i < 30; i++){
+  //   write_imaging_data();
   // }
 
+  std::thread thread_object (serial_receiving_thread);
+  while(running){
+    loop();
+  }
+
+  cout << "Closing servers" << endl;
+  server_1.close_server();
+  server_2.close_server();
+  thread_object.join();
+  thread_1.join();
+  thread_2.join();
+  
+  exit(1);
+
+  // LoRa_end(&modem);
   return 0;
 }
