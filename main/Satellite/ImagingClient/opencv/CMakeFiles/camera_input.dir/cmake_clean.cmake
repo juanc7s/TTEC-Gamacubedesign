849d76db25256d1433fef538fa5558a504c3dc91
Vitinho <aguirra.vitor@gmail.com>
2024-02-22 15:29:14 +0000

    Mudancas no codigo de imageamento

---
+++ main/Satellite/ImagingClient/opencv/CMakeFiles/camera_input.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/camera_input.dir/camera_input.cpp.o"
+  "camera_input"
+  "camera_input.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/camera_input.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main/Satellite/ImagingClient/opencv/CMakeFiles/teste-opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teste-opencv.dir/teste-opencv.cpp.o"
+  "teste-opencv"
+  "teste-opencv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teste-opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
